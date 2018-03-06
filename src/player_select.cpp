022ab4f8f7ceb02ff0e2063a37b2af5ea8b9f5f2
Simone Perriello <tigerjack89@gmail.com>
2018-03-06 21:02:04 +0100

    Added player_select file
    Signed-off-by: Simone Perriello <tigerjack89@gmail.com>

---
+++ src/player_select.cpp
@@ -0,0 +1,45 @@
+#include "button.h"
+#include <pthread.h>
+#include <miosix.h>
+#include <unistd.h>
+
+using namespace miosix;
+
+typedef Gpio<GPIOD_BASE,12> led4; //green
+typedef Gpio<GPIOD_BASE,13> led1; //orange
+typedef Gpio<GPIOD_BASE,14> led3; //red
+typedef Gpio<GPIOD_BASE,15> led2; //blue
+
+int chooseNumPlayers() {
+  int numPlayers = 2;
+
+  led1::high();
+  led2::high();
+  configureButtonInterrupt();
+
+  for(;;)
+    {
+      waitForButton();
+      numPlayers++;
+      if (numPlayers == 3)
+        led3::high();
+      else if (numPlayers == 4)
+        led4::high();
+      else {
+        numPlayers = 2;
+        led3::low();
+        led4::low();
+      }
+    }
+  return 0; // TODO
+}
+
+int player_select()
+{
+  led1::mode(Mode::OUTPUT);
+  led2::mode(Mode::OUTPUT);
+  led3::mode(Mode::OUTPUT);
+  led4::mode(Mode::OUTPUT);
+
+  chooseNumPlayers();
+}
