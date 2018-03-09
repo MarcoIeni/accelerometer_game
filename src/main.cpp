d6bf515275988a8cd9850c27ff56fae9f2f6b2e4
Simone Perriello <simone.perriello@protonmail.com>
2018-03-09 15:57:03 +0100

    Added first toy example of game logic
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include "player_select.h"
+#include "timer_logic.h"
 #include <miosix.h>
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
 
 using namespace miosix;
 
 int play(int players);
 
 int main() {
-  int players = player_select();
+  // int players = player_select();
+  int players = 2;
   play(players);
 
   return 0;
 }
 
-int play(int players) { return 0; }
+float abs(float val) {
+  return val >= 0 ? val : -val;
+}
+
+int play(int players) {
+  float *times = new float[players];
+  for (;;) {
+    for (int i = 0; i < players; ++i) {
+      printf("Player %d, let's play\n", i);
+      times[i] = abs(getElapsedTime() - 5);
+      printf("%f\n", times[i]);
+    }
+
+    int min = 0;
+    for (int i = 1; i < players; ++i) {
+      if (times[i] < times[min])
+        min = i;
+    }
+
+    printf("And the winner is %d\n", min);
+  }
+  return 0;
+}
