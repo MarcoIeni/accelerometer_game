82963011c2bae70161d679d529c71daa15c79530
Simone Perriello <simone.perriello@protonmail.com>
2018-04-16 14:34:32 +0200

    accelerometer: double tap working test
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include "lis3dsh_usage.h"
 #include "lis3dsh_sync.h"
+#include "lis3dsh_usage.h"
 #include <miosix.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+
+#define SLEEP_SEC 300
 
 using namespace miosix;
 
@@ -25,33 +27,22 @@ int main() {
 int player_select() { return 0; }
 
 void delay() {
-  for (int i = 0; i < 500; ++i) asm volatile ("nop");
+  for (int i = 0; i < 500; ++i)
+    asm volatile("nop");
 }
 
 void test_who_am_i() {
-  //delay();
+  // delay();
   unsigned char c = spi_get_data(0x0F);
-  printf("who_am_i: %x\n", c);
+  printf("Who_Am_I: %x\n", c);
 }
 
-void play(int players) {
-  // Simple first trial
-
-  led1::mode(Mode::OUTPUT);
-  led2::mode(Mode::OUTPUT);
-  led3::mode(Mode::OUTPUT);
-  led4::mode(Mode::OUTPUT);
-
+void test_double_tap() {
+  LIS3DSH_click_int_config();
 
   int numClicks = 1;
   led1::high();
 
-  spi_init();
-  LIS3DSH_init();
-  LIS3DSH_interrupt_config();
-
-  test_who_am_i();
-  LIS3DSH_click_int_config();
   for (;;) {
     waitForClick();
     printf("Wait end\n");
@@ -68,6 +59,23 @@ void play(int players) {
       led3::low();
       led4::low();
     }
+    Thread::sleep(SLEEP_SEC);
   }
+}
+
+void play(int players) {
+  // Simple first trial
+
+  led1::mode(Mode::OUTPUT);
+  led2::mode(Mode::OUTPUT);
+  led3::mode(Mode::OUTPUT);
+  led4::mode(Mode::OUTPUT);
+
+  spi_init();
+  LIS3DSH_init();
+  LIS3DSH_interrupt_config();
+
+  test_who_am_i();
+  test_double_tap();
   return;
 }
