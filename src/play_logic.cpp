cdf86a2bb851940eebdb135f822f41ea8306d2d8
Simone Perriello <simone.perriello@protonmail.com>
2018-04-30 14:11:20 +0200

    play_logic refactored; added comments.
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- src/play_logic.cpp
+++ src/play_logic.cpp
@@ -8,6 +8,33 @@ using namespace miosix;
 
 bool click_flag = false;
 
+/**
+ *  \brief Get the elapsed time between two interrupts, generated from the
+ * button or from the accelerometer.
+ *  \return The elapsed time
+ */
+float get_elapsed_time() {
+  Timer timer0;
+  wait_for_interrupt();
+  printf("Timer starting\n");
+  timer0.start();
+  Thread::sleep(SLEEP_MS);
+  wait_for_interrupt();
+  timer0.stop();
+  printf("Timer stopped\n");
+  Thread::sleep(SLEEP_MS);
+  float time = timer0.interval();
+  time /= TICK_FREQ;
+  timer0.clear();
+  printf("time:%0.3fs\n", time);
+  return time;
+}
+
+/**
+ *  \brief Return the absolute value
+ *  \param val the value for which to evaluate the absolute value.
+ *  \return the absolute value as a float.
+ */
 static float abs(float val) { return val >= 0 ? val : -val; }
 
 unsigned player_select() {
@@ -36,7 +63,7 @@ unsigned player_select() {
   }
 }
 
-int play(unsigned players) {
+void play(unsigned players) {
   float *times = new float[players];
   printf("Waiting for game to start\n");
   wait_for_interrupt();
@@ -57,7 +84,6 @@ int play(unsigned players) {
     }
     celebrate(winner);
   }
-  return 0;
 }
 
 void celebrate(unsigned player) {
@@ -65,21 +91,4 @@ void celebrate(unsigned player) {
   switch_off_all();
   player_blink(player, 7);
   toggle_all();
-}
-
-float get_elapsed_time() {
-  Timer timer0;
-  wait_for_interrupt();
-  printf("Timer starting\n");
-  timer0.start();
-  Thread::sleep(SLEEP_MS);
-  wait_for_interrupt();
-  timer0.stop();
-  printf("Timer stopped\n");
-  Thread::sleep(SLEEP_MS);
-  float time = timer0.interval();
-  time /= TICK_FREQ;
-  timer0.clear();
-  printf("time:%0.3fs\n", time);
-  return time;
 }
--- src/play_logic.h
+++ src/play_logic.h
@@ -5,9 +5,22 @@
 #define MIN_PLAYERS 2
 #define MAX_PLAYERS 4
 
+/**
+ *  \brief Allow the selection of the number of players for the game using the
+ * onboard button
+ *  \return the number of players selected.
+ */
 unsigned player_select();
+
+/**
+ *  \brief Celebrate the winner of the game.
+ *  \param player The winning player
+ */
 void celebrate(unsigned player);
-int play(unsigned players);
-float get_elapsed_time();
+/**
+ *  \brief Starts the game with a given number of players.
+ *  \param players The number of players joining the game.
+ */
+void play(unsigned players);
 
 #endif
