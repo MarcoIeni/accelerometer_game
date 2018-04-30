4f46be15b87c39a5be22cf5398cb2e91e57ec682
Simone Perriello <simone.perriello@protonmail.com>
2018-04-30 14:11:38 +0200

    lis3dsh_manager: added comments
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- src/lis3dsh_manager.cpp
+++ src/lis3dsh_manager.cpp
@@ -5,22 +5,15 @@
 
 using namespace miosix;
 
-/*!< SPI clock enable (CE or CS) on PE.3*/
+/*! SPI clock enable (CE or CS) on PE.3 */
 typedef Gpio<GPIOE_BASE, 3> spi_ce;
-/*!< SPI clock on PA.5*/
+/*! SPI clock on PA.5 */
 typedef Gpio<GPIOA_BASE, 5> spi_sck;
-/*!< SPI input on PA.7*/
+/*! SPI input on PA.7 */
 typedef Gpio<GPIOA_BASE, 7> spi_si;
-/*!< SPI output on PA.6*/
+/*! SPI output on PA.6 */
 typedef Gpio<GPIOA_BASE, 6> spi_so;
 
-/**
- *  \brief Sends 1 byte to the accelerometer through SPI and returns the value
- *
- *  \param data the data to be written
- *  \param address the address to write into
- *  \return void
- */
 unsigned char spi_send_recv(unsigned char byte) {
   // Wait for TXE
   while (!(SPI1->SR & 0b10))
@@ -32,13 +25,6 @@ unsigned char spi_send_recv(unsigned char byte) {
   return (SPI1->DR);
 }
 
-/**
- *  \brief Writes 1 byte to the accelerometer through SPI
- *
- *  \param data the data to be written
- *  \param address the address to write into
- *  \return void
- */
 void spi_send_data(unsigned char data, unsigned char address) {
   spi_ce::low();
   spi_send_recv(address);
@@ -56,6 +42,20 @@ unsigned char spi_get_data(unsigned char address) {
   return c;
 }
 
+/**
+ - Enable spi clock to PE3, PA5, PA6 and PA7.
+ - Set PE3 mode to output
+ - Set PA5, PA6, PA7 to alternate function mode.
+ - Enable clock to SPI1
+ - Set the following configuration parameters (all the others to default):
+     -# CPHA: 1
+     -# CPOL: 1,
+     -# Master Mode
+     -# Baud Rate: maximum frequency/32 (i.e. 10 Mhz/32)
+     -# SSI enabled
+     -# SSM enabled
+     -# SPI enabled
+ */
 void spi_init() {
   // Enable spi clock to PE3, PA5, PA6 and PA7.
   // Set PE3 mode to output
@@ -72,7 +72,8 @@ void spi_init() {
 
   // Enable clock to SPI1
   RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-  SPI1->CR1 = 1;           //  CPHA=1
+
+  SPI1->CR1 = 1;           // CPHA=1
   SPI1->CR1 |= 1 << 1;     // CPOL=1,
   SPI1->CR1 |= 1 << 2;     // Master Mode
   SPI1->CR1 |= 0b001 << 5; // Baud Rate = maximum frequency/32 (i.e. 10 Mhz/32)
@@ -82,65 +83,63 @@ void spi_init() {
 }
 
 /**
- *  \brief Initialize LIS3DSH
+ * Write:
+ *  -  to MEMS CTRL_REG4 register:
+ *    -# Output data rate: 100 Hz
+ *    -# Block data update: Continuous update
+ *    -# Enable xen, yen, zen (x, y, z) axes
+ *  - to MEMS CTRL_REG5 register
+ *    -# Anti-aliasing filter bandwidth: 800Hz
+ *    -# Full scale selection: +- 2g
+ *    -# Self-test: disabled
+ * All other values are left to default ones.
  *
- *  First initialize accelerometer SPI.
- *  Then write to MEMS CTRL_REG4 and MEMS CTRL_REG5 registers
- *
- *  \return return void
  */
 void LIS3DSH_init() {
-  /*
-    Addr: 0x20
-    Output data rate = 0b0110= 100Hz
-    Block data update: continuous update
-    Enable zen, yen, xen (z, y, x axes)
-   */
   spi_send_data(0b01100111, LIS3DSH_CTRL_REG4_ADDR);
-
-  /* Write value to MEMS CTRL_REG5 register
-    All defaults:
-    Antialiasing 800 Hz
-    Self-test disabled
-    +- 2g
-  */
   spi_send_data(0b0, LIS3DSH_CTRL_REG5_ADDR);
 }
 
-/**
- *  \brief Configure interrupt
- *
- *  \return void
- */
+/* Write
+   - to MEMS CTRL_REG3 register:
+   -# Interrupt signal active high
+   -# Interrupt signal latched
+   -# Interrupt1 disabled
+   -# Interrupt2 enabled
+   - to MEMS CTRL_REG1 register:
+   -# State machine 1 enabled
+   -# State machine 1 interrupt routed to INT2
+   -# Hysteresis: 0
+*/
 void LIS3DSH_interrupt_config() {
-  /*
-    Configure accelerometer INT2
-  */
-  /* Write value to MEMS CTRL_REG3 register (Address 0x23)
-     Interrupt signal active high
-     Interrupt signal latched
-     Interrupt1 disabled
-     Interrupt2 enabled
-  */
   spi_send_data(0b01010000, LIS3DSH_CTRL_REG3_ADDR);
-
-  /* Configure State Machine 1 */
-  /* Write value to MEMS CTRL_REG1 register
-     SM1 Enable; SM1 interrupt routed (by default) to INT1; hysteris 0
-  */
   spi_send_data(0b00001001, LIS3DSH_CTRL_REG1_ADDR);
 }
+
 /**
- *  \brief Configure click interrupt
- *
- *  Configure the states of the State Machine 1 (SM1) of the LIS3DSH in order to
- *  recognize single click events.
- *
- *  \return void
+ Configure the states of the State Machine 1 (SM1) of the LIS3DSH in order to
+ recognise single click (tap) events.
+ The state machine was configured starting from the application note provided
+ by ST for the LIS3DSH 3d-axial accelerometer. The original configuration was
+ thought for a double tap; we have adapted it to be used for a single tap.
+
+ The first two states (GNTH1 | TI1) are used to create a “pre-silence” time
+ window, the NEXT condition is the Timer1. If one or more axes become greater
+ than threshold 1, the RESET condition is satisfied and the program pointer is
+ reset.
+
+ After that there is the real first tap detection composed of two states, the
+ first one is composed of (NOP | GNTH2), where the next condition is satisfied
+ when one or more triggered axes exceed threshold 2. In the second state (TI3 |
+ LNTH2), it is checked whether the acceleration value on the axis becomes lower
+ than threshold 2 within a time defined by Timer 3. The result of these states
+ is to create a time window (TI3) where the acceleration is first higher, and
+ then lower, than threshold 2.
+
+ \image html single_click_output.png "Single click output"
  */
 void LIS3DSH_click_sm_config() {
 
-  /* Set LIS3DSH State Machines configuration */
   // configure general timer TC1 for SM1
   // Register TIMx_1 (x=1..4) define the initial value of the timer counter TC1
   spi_send_data(0x02, LIS3DSH_TIM4_1_ADDR);
@@ -160,22 +159,24 @@ void LIS3DSH_click_sm_config() {
      GNTH1 - TI1
    */
   spi_send_data(0x51, LIS3DSH_ST1_1_ADDR);
+
   // 0b 0101 0001
   // as before
   spi_send_data(0x51, LIS3DSH_ST1_2_ADDR);
+
   // 0b 0000 0110
   /* NOP - GNTH2
    */
   spi_send_data(0x06, LIS3DSH_ST1_3_ADDR);
+
   /* 0b 0011 1000
      TI3 - LNTH2
      GNTH1
    */
+
   spi_send_data(0x38, LIS3DSH_ST1_4_ADDR);
-  /*
-    The following was used for double-click
-   */
-  /* 0b 0000 0100
+
+  /* 0b 0000 0011
      NOP - TI4
    */
   spi_send_data(0x11, LIS3DSH_ST1_5_ADDR);
